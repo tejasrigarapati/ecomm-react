30dd6dd8b693450df8604c198aff3fd3631b47ce
npx <tejasrigarapatilyros@gmail.com>
2024-03-05 17:28:29 +0530

    alert

---
+++ src/App7.js
@@ -0,0 +1,11 @@
+import react from "react";
+export default function App7(){
+    const handleClick=()=>{
+        alert("hrlo");
+    };
+    return(
+        <div>
+            <button  onClick={handleClick}>Click</button>
+        </div>
+    )
+    }
