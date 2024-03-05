32ffeaa329b1e943287627c04ff3b67ce49080bd
npx <tejasrigarapatilyros@gmail.com>
2024-03-05 10:12:15 +0530

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
 function App() {
+  const [products] = useState([
+     { name: "Live-1", description:  "Live live magazines bring events to life with vivid imagery and real-time reporting.", price: 1, image: "a.jpg", button: "Add" },
+      { name: "Live-2", description: "Experience the pulse of the moment with live live magazines, capturing the essence of unfolding stories.", price: 2, image: "b.jpg", button: "Add" },
+      { name: "Live-3", description: "Engage in dynamic storytelling with live live magazines, offering immersive experiences beyond the page.", price: 3, image: "c.jpg", button: "Add" },
+      { name: "Live-4", description: "Stay connected to the world's happenings with live live magazines, delivering news as it unfolds.", price: 4, image: "d.jpg", button: "Add" },
+      { name: "Live-5", description: "Immerse yourself in the now with live live magazines, offering a glimpse into the heartbeat of society.", price: 5, image: "e.jpg", button: "Add" },
+      { name: "Live-6", description: "From concerts to conferences, live live magazines provide a front-row seat to the world's events in real-time.", price: 6, image: "f.jpg", button: "Add" },
+  ]);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Coffee Shop</h1>
+      <hr />
+      <div className="main">
+        {products.map((product,index) => (
+          <div key={index} className='container'>
+            <img width='250' src={product.image} alt={product.name} />
+            <div>{product.name}</div>
+            <div>{product.description}</div>
+            <div>Price: ${product.price}</div>
+            <button>Add</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
